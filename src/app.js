00aa20c74ac4427fe24fcf6b02a60e005c99a797
shreebaddi <74357250+shreebaddi@users.noreply.github.com>
2021-01-02 18:57:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,23 +2,49 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
-
+function moreAboutHome(address, distanceFromTown, hasNeighbours){
+    var text = typeof(address)+typeof(distanceFromTown)+typeof(hasNeighbours)
+    return text
+    }
+    
+    
 //Trial 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily){
+    if(typeof(parents)=="string" && typeof(noOfSiblings)=="number" && typeof(isNuclearFamily)=="boolean"){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
 
 //Trial 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
-
+function doesFriendExist(ageInText, ageInNumber){
+    if(typeof(ageInText=='NaN'))
+    if(isNaN(ageInText)){
+    return ageInText
+    }
+    else{
+    return ageInNumber
+    }
+    }
 //Trial 4:
 //Lily gave Karen x sweets
 //Karen ate y sweets herself
 //On her way to the river, she ate another z sweets every n meters travelled
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel){
+    var remaining=(totalNoOfSweets-(sweetsConsumedByKaren+sweetsConsumedInNMeters*metersToTravel))/2;
 
+    return remaining;
+}
 //Trial 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
